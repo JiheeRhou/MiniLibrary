045e526eb5fa96cba4c9d42945aa18fd1cd931a0
JiheeRhou <200521364@student.georgianc.on.ca>
2023-07-28 19:40:26 -0400

    Scaffolding completed

---
+++ Controllers/CheckoutsController.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MiniLibrary.Data;
+using MiniLibrary.Models;
+
+namespace MiniLibrary.Controllers
+{
+    public class CheckoutsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CheckoutsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Checkouts
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Checkouts.Include(c => c.Book).Include(c => c.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Checkouts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Checkouts == null)
+            {
+                return NotFound();
+            }
+
+            var checkout = await _context.Checkouts
+                .Include(c => c.Book)
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (checkout == null)
+            {
+                return NotFound();
+            }
+
+            return View(checkout);
+        }
+
+        // GET: Checkouts/Create
+        public IActionResult Create()
+        {
+            ViewData["BookId"] = new SelectList(_context.Books, "Id", "ISBN");
+            ViewData["UserId"] = new SelectList(_context.Set<User>(), "Id", "Id");
+            return View();
+        }
+
+        // POST: Checkouts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,UserId,BookId,StartDate,EndDate,IsReturn")] Checkout checkout)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(checkout);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BookId"] = new SelectList(_context.Books, "Id", "ISBN", checkout.BookId);
+            ViewData["UserId"] = new SelectList(_context.Set<User>(), "Id", "Id", checkout.UserId);
+            return View(checkout);
+        }
+
+        // GET: Checkouts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Checkouts == null)
+            {
+                return NotFound();
+            }
+
+            var checkout = await _context.Checkouts.FindAsync(id);
+            if (checkout == null)
+            {
+                return NotFound();
+            }
+            ViewData["BookId"] = new SelectList(_context.Books, "Id", "ISBN", checkout.BookId);
+            ViewData["UserId"] = new SelectList(_context.Set<User>(), "Id", "Id", checkout.UserId);
+            return View(checkout);
+        }
+
+        // POST: Checkouts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,UserId,BookId,StartDate,EndDate,IsReturn")] Checkout checkout)
+        {
+            if (id != checkout.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(checkout);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CheckoutExists(checkout.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BookId"] = new SelectList(_context.Books, "Id", "ISBN", checkout.BookId);
+            ViewData["UserId"] = new SelectList(_context.Set<User>(), "Id", "Id", checkout.UserId);
+            return View(checkout);
+        }
+
+        // GET: Checkouts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Checkouts == null)
+            {
+                return NotFound();
+            }
+
+            var checkout = await _context.Checkouts
+                .Include(c => c.Book)
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (checkout == null)
+            {
+                return NotFound();
+            }
+
+            return View(checkout);
+        }
+
+        // POST: Checkouts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Checkouts == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Checkouts'  is null.");
+            }
+            var checkout = await _context.Checkouts.FindAsync(id);
+            if (checkout != null)
+            {
+                _context.Checkouts.Remove(checkout);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CheckoutExists(int id)
+        {
+          return (_context.Checkouts?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
